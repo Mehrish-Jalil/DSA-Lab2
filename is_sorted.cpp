a4bd0122373dce3b48946f189ae5a4dc33289969
Mehrish-Jalil <mehrishjalil2208@gmail.com>
2026-09-18 18:38:38 +0500

    Add fixed  implementation

--- is_sorted.cpp
+++ is_sorted.cpp
@@ -46,7 +46,9 @@ void testMiddleUnsorted() {
 }
 bool isSorted(const int* arr, const int size) {
     if (size <= 1) return true;
-    if (arr[0] > arr[1]) return false; 
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i + 1]) return false;
+    }
     return true;
 }
 // MAIN 
